024c34a9bf823b6a6c5c095693ab2591c5b8770f
Ramprit Sahani <rampritsahani@protonmail.com>
2019-11-21 08:43:36 +0530

    first commit

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,57 @@
+﻿import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  Container,
+  DropdownItem
+} from "reactstrap";
+
+//TODO: Web Template Studio: Add a new link in the NavBar for your page here.
+// A skip link is included as an accessibility best practice. For more information visit https://www.w3.org/WAI/WCAG21/Techniques/general/G1.
+export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <React.Fragment>
+      <Navbar color="primary" dark expand="md">
+        <Container>
+          <NavbarBrand href="/">Saint Gobain</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/List">List</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>Option 1</DropdownItem>
+                  <DropdownItem>Option 2</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Reset</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </React.Fragment>
+  );
+}
